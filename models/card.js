2029c97d340a86730fc6d303b12a3d000806aadc
Steven Mercer <stevenmercer9913@gmail.com>
2021-02-14 18:34:32 -0800

    shuffle complete

--- models/card.js
+++ models/card.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const cardSchema = new mongoose.Schema({
     name: {type:String, required: true, default: 'King of Hearts'},
-    image: { type: String, default: 'https://media.istockphoto.com/vectors/king-of-hearts-playing-card-in-blue-vector-id165644140?b=1&k=6&m=165644140&s=170667a&h=5QkknhWw6pA0K3HXkAXxfOKhSL3KuxVOG4v56XSILpQ=' }
+    image: { type: String, default: 'https://media.istockphoto.com/vectors/king-of-hearts-playing-card-in-blue-vector-id165644140?b=1&k=6&m=165644140&s=170667a&h=5QkknhWw6pA0K3HXkAXxfOKhSL3KuxVOG4v56XSILpQ=' },
+    isFlipped: {type: Boolean, default: false}
 })
 
 const Card = mongoose.model('Card', cardSchema)
