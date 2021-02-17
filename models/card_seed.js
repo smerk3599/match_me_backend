976e9645cf75950030686385bbf72b1c1c91c6fb
Steven Mercer <stevenmercer9913@gmail.com>
2021-02-16 19:10:52 -0800

    seed data

---
+++ models/card_seed.js
@@ -0,0 +1,182 @@
+module.exports = [
+  {
+      name: 'Rainbow Animals',
+      front: 'https://a-z-animals.com/media/2021/01/mammals-400x300.jpg',
+      back: 'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name: 'Rainbow Animals',
+      front: 'https://a-z-animals.com/media/2021/01/mammals-400x300.jpg',
+      back: 'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Siberian Tiger',
+      front:'https://www.cdc.gov/coronavirus/2019-ncov/images/daily-life-coping/tiger-medium.JPG',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Siberian Tiger',
+      front:'https://www.cdc.gov/coronavirus/2019-ncov/images/daily-life-coping/tiger-medium.JPG',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Emperor Penguins',
+      front:'https://www.vpr.org/sites/vpr/files/styles/medium/public/202001/emperor-penguins-istock-Mario_Hoppmann.png',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Emperor Penguins',
+      front:'https://www.vpr.org/sites/vpr/files/styles/medium/public/202001/emperor-penguins-istock-Mario_Hoppmann.png',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Baby Panda',
+      front:'https://i.pinimg.com/originals/82/b7/7c/82b77c6d6d0ffca41751314401014aee.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Baby Panda',
+      front:'https://i.pinimg.com/originals/82/b7/7c/82b77c6d6d0ffca41751314401014aee.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Koala Bear',
+      front:'https://media.goeco.org/_media/media/69/15154_455_300.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Koala Bear',
+      front:'https://media.goeco.org/_media/media/69/15154_455_300.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Elephants',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22sIqG-icbFxtAwPfCkKLjw3CAu9Ctzq3_A&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Elephants',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22sIqG-icbFxtAwPfCkKLjw3CAu9Ctzq3_A&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Lion',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNCksI6l2anhTHZ3_TgesCOYrS6H-rgySXzA&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Lion',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNCksI6l2anhTHZ3_TgesCOYrS6H-rgySXzA&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Aramdillo',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLLgx-Qds6Cif8-HdcnDMJS6D2UMh9DXZXhQ&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Armadillo',
+      front:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLLgx-Qds6Cif8-HdcnDMJS6D2UMh9DXZXhQ&usqp=CAU',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Kangaroo',
+      front:'https://images.newscientist.com/wp-content/uploads/2014/07/dn25829-2_800.jpg?width=600https://images.newscientist.com/wp-content/uploads/2014/07/dn25829-2_800.jpg?width=600',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Kangaroo',
+      front:'https://images.newscientist.com/wp-content/uploads/2014/07/dn25829-2_800.jpg?width=600',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Sea Turtle',
+      front:'https://cdn.wallpapersafari.com/72/81/BoKtOD.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Sea Turtle',
+      front:'https://cdn.wallpapersafari.com/72/81/BoKtOD.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Octopus',
+      front:'https://i.pinimg.com/originals/29/38/f1/2938f108e61fb51cf6e5bfbe0ef9f55a.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Octopus',
+      front:'https://i.pinimg.com/originals/29/38/f1/2938f108e61fb51cf6e5bfbe0ef9f55a.jpg',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Frog',
+      front:'https://images.unsplash.com/photo-1532367903698-f2577574e8ea?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDk5fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Frog',
+      front:'https://images.unsplash.com/photo-1532367903698-f2577574e8ea?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDk5fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Horse',
+      front:'https://images.unsplash.com/photo-1580065753380-ed9eda9e46c0?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Horse',
+      front:'https://images.unsplash.com/photo-1580065753380-ed9eda9e46c0?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Butterflies',
+      front:'https://unsplash.com/photos/IZOf4LVoR-o',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Butterflies',
+      front:'https://unsplash.com/photos/IZOf4LVoR-o',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Jellyfish',
+      front:'https://images.unsplash.com/photo-1612676880381-2bc5367a2ad7?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  },
+  {
+      name:'Jellyfish',
+      front:'https://images.unsplash.com/photo-1612676880381-2bc5367a2ad7?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fEpwZzZLaWRsLUhrfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+      back:'https://i.pinimg.com/originals/b8/11/a5/b811a58a20c1b74d1179f44771747d76.jpg',
+      status: 'back'
+  }
+]
